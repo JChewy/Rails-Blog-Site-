a49b8a36fb051c31b2a7875a7059dce3616176e0
Jeff <retroh3at@gmail.com>
2015-12-25 22:10:46 -0500

    added users new and edit view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
 
   resources :home
   resources :articles
+  resources :users
+
+  get 'signup', to: 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
